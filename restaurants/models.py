1089021fc3c34663ef513a513d90dff0f956700f
Sana AlKhaled <sanaalkhaled@Sanas-MacBook-Pro.local>
2020-01-28 12:49:31 +0300

    taskfourcomplete

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 120)
+    description = models.TextField()
+    opening_time= models.TimeField()
+    closing_time = models.TimeField()
+
+    def __str__(self):
+        return "%d, by: %s" %(self.id,self.name)
